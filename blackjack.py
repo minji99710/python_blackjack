8b450fb05e19dcd7e1649d05345f53084bdc72ab
unknown <minji99710>
2022-02-08 16:15:16 +0900

    Add greeting and take name input

---
+++ blackjack.py
@@ -0,0 +1,14 @@
+#blackjack game
+
+#start : greet, make card deck, shuffle the card 
+greeting = 'Hello, welcome to bichon casino! Let me introduce your dealer today, kimbision.'
+print(greeting)
+
+username = input('What is your name, sir? : ')
+print('\nNice to meet you {name}, it\'s a perfect day to gamble! \
+\nHave a good time with your puriti gambling bichon :)'.format(name=username))
+
+
+
+
+
